199c99e0966c07d737d49b3c7a7991c4e705c3ca
Satyam Tomar <satyam321tomar@gmail.com>
2024-10-17 16:23:45 +0530

    set-up tailwind.config, custom colors and fonts added and also favicon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div className='bg-red-600'>App</div>
-  )
+    <div className="bg-gradient-to-tr from-primary-dark  to-primary-dark2 w-full h-screen">
+      <h1 className="sub_heading text-white">Hello</h1>
+      <h1 className="heading active ">Hello</h1>
+      <h1 className="base_heading active text-primary-pink">Hello</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,23 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", 
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-dark": "#19121F",
+        "primary-dark2": "#372743",
+        "primary-pink": "#DD0CFF",
+        "primary-indigo": "#27123C",
+        "primary-indigo-light": "#372743",
+      },
+      fontFamily: {
+        "CabinCondensed": ["Cabin Condensed", "Sarif"],
+        "Poppins": ["Poppins", "Sarif"]
+      },
+
+    },
   },
   plugins: [],
 }
