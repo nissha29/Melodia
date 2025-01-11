50436c49ad355514dd17751df093b3d6e15d4716
ruchap24 <patilrucha991@gmail.com>
2025-01-11 10:01:11 +0530

    reverted

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,18 +4,15 @@ import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+
 
   function openMenuBar(){
     setIsOpen((prev)=> !prev)
   }
 
-  function toggleTheme() {
-    setIsDarkMode((prev) => !prev);
-  }
 
   return (
-    <div className={`relative rounded-lg h-20 2xl:mx-2xl-screen xl:mx-xl-screen lg:mx-lg-screen md:mx-md-screen sm:mx-sm-screen mx-5 ${isDarkMode ? 'bg-green-800' : 'bg-white'}`}>
+    <div className='relative bg-secondary-bg rounded-lg h-20 2xl:mx-2xl-screen xl:mx-xl-screen lg:mx-lg-screen md:mx-md-screen sm:mx-sm-screen mx-5'>
         <div className='flex justify-between text-white md:px-16 md:py-3 sm:px-9 sm:py-4 px-6 py-4 font-playwrite'>
             <div className='md:text-3xl sm:text-2xl text-2xl pt-2'>Melodia</div>
             <div className='md:hidden block pt-1 hover:cursor-pointer'>
@@ -25,12 +22,12 @@ function Navbar() {
                 <NavLink to={'/signup'} className='pt-2 hover:cursor-pointer'>Signup</NavLink>
                 <NavLink to={'/signin'} className='bg-white rounded-full md:px-6 md:py-3 sm:px-5 sm:py-3 text-secondary-text hover:bg-[#e9e9e9] hover:cursor-pointer'>Login</NavLink>
             </div>
-            <button 
+            {/* <button 
               onClick={toggleTheme} 
               className={`ml-4 p-2 rounded-full ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-gray-200 text-black'} hover:bg-gray-300 transition-colors duration-300`}
             >
               {isDarkMode ? 'Light Mode' : 'Dark Mode'}
-            </button>
+            </button> */}
         </div>
         {isOpen && (
         <div className='md:hidden absolute right-10 top-[1.2rem] font-playwrite text-[1rem] text-center bg-white text-secondary-text flex flex-col gap-2 px-5 py-3 rounded-sm mt-10 z-10'>
@@ -38,6 +35,7 @@ function Navbar() {
             Signup
           </NavLink>
           <NavLink to={'/signin'} className='px-5 py-1 rounded-lg hover:bg-[#d9bce036] text-secondary-text transition-colors duration-200'>
+          Login
           </NavLink>
         </div>
         )}
@@ -45,4 +43,4 @@ function Navbar() {
   )
 }
 
-export default Navbar;
+ export default Navbar
